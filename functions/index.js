a04ecd27221d8785957d55b87b1ca0a2ec16533c
Todd Albert <todd@bocacode.com>
2023-02-20 16:51:33 -0500

    added login

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import functions from "firebase-functions"
 import express from "express"
 import cors from "cors"
-import { signup } from "./src/users.js"
+import { signup, login } from "./src/users.js"
 
 const app = express()
 app.use(cors())
@@ -9,5 +9,6 @@ app.use(express.json())
 
 // login and signup routes...
 app.post("/signup", signup)
+app.post("/login", login)
 
 export const api = functions.https.onRequest(app)
--- functions/src/users.js
+++ functions/src/users.js
@@ -15,3 +15,25 @@ export function signup(req, res) {
       res.status(500).send({ success: false, message: err.message })
     });
 }
+
+export function login(req, res) {
+  const { email, password } = req.body;
+  const db = dbConnect();
+  db.collection('users')
+    .where('email', '==', email.toLowerCase())
+    .where('password', '==', password)
+    .get()
+    .then(collection => {
+      const users = collection.docs.map(doc => ({...doc.data(), userId: doc.id }))
+      if(!users.length) {
+        res.status(401).send({ success: false, message: 'User not found' })
+        return
+      }
+      let user = users[0]
+      user.password = undefined
+      res.status(200).send(user)
+    })
+    .catch(err => {
+      res.status(500).send({ success: false, message: err.message })
+    });
+}
